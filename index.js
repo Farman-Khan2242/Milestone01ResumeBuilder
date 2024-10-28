fd2e1544b71d91e7f629ef930365b000dd5e897f
FarmanKhan2242 <farman03170218621@gmail.com>
2024-10-28 21:24:27 +0500

    updatef files

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let eduList = document.getElementById('eduList');      //unordered of education
 let btn01 = document.getElementById('btn01');         //education button
+let conditionEdu = 0;
+let conditionSkill = 0;
+let conditionExp = 0;
 btn01.addEventListener('click',function(){
-    if(eduList.style.display == 'block'){
+    if(conditionEdu == 0){
+        conditionEdu++
         eduList.style.display = 'none';
         btn01.innerHTML = 'Show Education';
     }
     else {
+        conditionEdu--
         eduList.style.display = 'block';
         btn01.innerHTML = 'Hide Education'
     }
@@ -15,25 +20,28 @@ btn01.addEventListener('click',function(){
 let skillsList = document.getElementById('skillsList');  //unordered of skills
 let btn02 = document.getElementById('btn02');           // button
 btn02.addEventListener('click',function(){
-    if(skillsList.style.display == 'block'){
+    if(conditionSkill == 0){
+        conditionSkill++
         skillsList.style.display = 'none';
         btn02.innerHTML = 'Show Skills';
     }
     else {
+        conditionSkill--
         skillsList.style.display = 'block'
         btn02.innerHTML = 'Hide Skills'
     }
 })
 
-
 let expList = document.getElementById('expList');       //list of experiance
 let btn03 = document.getElementById('btn03');           //button
 btn03.addEventListener('click',function(){
-    if(expList.style.display == 'block'){
+    if(conditionExp == 0 ){
+        conditionExp++
        expList.style.display = 'none'
         btn03.innerHTML = 'Show Experiance';
     }
     else {
+        conditionSkill--
         expList.style.display = 'block';
         btn03.innerHTML = 'Hide Experiance';
     }
